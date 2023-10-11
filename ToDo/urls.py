b50c3837450df3182077768185707923eb712b44
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-11 10:35:46 -0500

    edit functionality

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -6,4 +6,7 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
     path('addTask/', views.add_task, name='addTask'),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task')
 ]
